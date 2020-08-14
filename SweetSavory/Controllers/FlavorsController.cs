5224f081c276ea21653febe6065d6eb047e6886f
Sara Kane <kane.sarak@gmail.com>
2020-08-14 15:50:28 -0700

    Add Flavor Create view and GET and POST Create methods in FlavorsController

--- SweetSavory/Controllers/FlavorsController.cs
+++ SweetSavory/Controllers/FlavorsController.cs
@@ -25,5 +25,22 @@ namespace SweetSavory.Controllers
     {
       return View(_db.Flavors.ToList());
     }
+
+    [Authorize]
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Flavor flavor)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      flavor.User = currentUser;
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
